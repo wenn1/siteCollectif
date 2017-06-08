a49fa28e80b33532f4e39d4b12585637c528d868
wenn1 <nolwenn.repellin@orange.fr>
2017-06-08 22:13:38 +0200

    Create partenaires.php

--- partenaires.php
+++ partenaires.php
@@ -15,27 +15,26 @@
 			<nav class="menu" role="navigation">
 			<?php $nav_en_cours = 'partenaires'; ?>
 			<?php include('include/inc_menu.php') ?>
-
 			</nav>
 		
 		<article  role="main">
 		<div class="logos clear">
 			
-				<a href="http://www.meef26.org" ><img id="log1" src="logos/logo1.jpg" alt="la meef 26" ></a>
-				<a href="http://www.vercors.org"><img id="log2" src="logos/logo2.jpg" alt="c.c.Massif du Vercors" ></a>
-				<a href="http://www.cc-royans-vercors.org"><img id="log3" src="logos/logo3.png" alt="c.c.Pays du Royans"></a>
-				<a href="http://www.communaute.sud-gresivaudan.org"><img id="log4" src="logos/logo8.jpg" alt="c.c.sud Grésivaudan" ></a>
-				<a href="http://www.europe-centre.eu/"><img id="log5" src="logos/logo5.jpg" alt="Programme LEADER"></a>
-				<a href="https://www.legrand.fr"><img id="log6" src="logos/logo6.png" alt="Société Legrand"> </a>
-				<a href="http://www.emploi-pvsg.org"><img id="log7" src="logos/logo7.jpg" alt="Maison de l'Emploi" ></a>
-				<a href="http://vercors.net/"><img id="log8" src="logos/logo4.jpg" alt="c.c.du Vercors" ></a>
+			<a href="http://www.meef26.org" ><img id="log1" src="logos/logo1.jpg" alt="la meef 26" ></a>
+			<a href="http://www.vercors.org"><img id="log2" src="logos/logo2.jpg" alt="c.c.Massif du Vercors" ></a>
+			<a href="http://www.cc-royans-vercors.org"><img id="log3" src="logos/logo3.png" alt="c.c.Pays du Royans"></a>
+			<a href="http://www.communaute.sud-gresivaudan.org"><img id="log4" src="logos/logo8.jpg" alt="c.c.sud Grésivaudan" ></a>
+			<a href="http://www.europe-centre.eu/"><img id="log5" src="logos/logo5.jpg" alt="Programme LEADER"></a>
+			<a href="https://www.legrand.fr"><img id="log6" src="logos/logo6.png" alt="Société Legrand"> </a>
+			<a href="http://www.emploi-pvsg.org"><img id="log7" src="logos/logo7.jpg" alt="Maison de l'Emploi" ></a>
+			<a href="http://vercors.net/"><img id="log8" src="logos/logo4.jpg" alt="c.c.du Vercors" ></a>
 			
 		</div>	
 		</article>
 
 
-			<footer id="footer" role="contentinfo"><?php include('include/inc_footer.php') ?>
-			</footer>
+		<footer id="footer" role="contentinfo"><?php include('include/inc_footer.php') ?>
+		</footer>
 		</div>
 	</body>
 </html>
